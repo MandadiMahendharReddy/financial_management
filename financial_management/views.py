4efa92d3a0dead553addc36b1e73208d6f13df9a
Mandadi Mahendhar Reddy <mandadimahendharreddy8523@gmail.com>
2025-09-13 19:51:31 +0530

    redirect page changes on text and admin redirect removed

--- financial_management/views.py
+++ financial_management/views.py
@@ -18,8 +18,10 @@ from savings.models import Saving
 from django.db import models
 
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 
 @login_required
+@never_cache
 def home(request):
     total_income = Income.objects.filter(user=request.user).aggregate(total=models.Sum("amount"))["total"] or 0
     total_expenditure = Expenditure.objects.filter(user=request.user).aggregate(total=models.Sum("amount"))["total"] or 0
